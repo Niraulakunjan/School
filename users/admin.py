74969c3137f007877b8b9df7b07a8e6ebca29a8b
Kunjan Niraula <niraula67kunjan@gmail.com>
2026-04-13 00:34:22 +0545

    kunjan

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Fieldsets for user editing
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Information', {'fields': ('role', 'phone_number', 'address')}),
+    )
+    # Fieldsets for user creation
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role Information', {'fields': ('role', 'phone_number', 'address')}),
+    )
+    # Fields to display in the list view
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
+    # Filter users in the admin list view by role and staff status
+    list_filter = ('role', 'is_staff', 'is_superuser', 'is_active')
+    # Searchable fields
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
