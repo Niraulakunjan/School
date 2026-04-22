b7b5ebb08ad4ce497055611d82124293eabe9a52
Kunjan Niraula <niraula67kunjan@gmail.com>
2026-04-22 22:32:31 +0545

    feat: enforce tenant-specific database operations and queryset validation in SchoolSettingViewSet

--- school_settings/views.py
+++ school_settings/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import SchoolSetting
 from .serializers import SchoolSettingSerializer
+from utils.tenant_utils import get_current_tenant_db
+from finance.models import FinancialYear
 
 class SchoolSettingViewSet(viewsets.ViewSet):
     """
@@ -10,20 +12,36 @@ class SchoolSettingViewSet(viewsets.ViewSet):
     POST will update the record.
     """
     def list(self, request):
-        setting, created = SchoolSetting.objects.get_or_create(id=1)
-        # If created, maybe set a default name from the tenant?
-        if created:
-             # We can't easily get the tenant name here without the meta-db,
-             # but django-tenants should have the current tenant name if it's set in the meta model.
-             # For now, default is fine.
-             pass
+        db = get_current_tenant_db()
+        if db == 'default':
+            return Response({'detail': 'Tenant context required.'}, status=status.HTTP_400_BAD_REQUEST)
+            
+        setting, created = SchoolSetting.objects.using(db).get_or_create(id=1)
         serializer = SchoolSettingSerializer(setting, context={'request': request})
         return Response(serializer.data)
 
     def create(self, request):
-        setting, _ = SchoolSetting.objects.get_or_create(id=1)
+        db = get_current_tenant_db()
+        if db == 'default':
+            return Response({'detail': 'Tenant context required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Get existing or create default (id=1 is used as singleton)
+        setting, _ = SchoolSetting.objects.using(db).get_or_create(id=1)
+        
         serializer = SchoolSettingSerializer(setting, data=request.data, partial=True, context={'request': request})
+        
+        # Explicitly set querysets for ForeignKey fields to the tenant DB for validation
+        if 'academic_year' in serializer.fields:
+            serializer.fields['academic_year'].queryset = FinancialYear.objects.using(db).all()
+        if 'fiscal_year' in serializer.fields:
+            serializer.fields['fiscal_year'].queryset = FinancialYear.objects.using(db).all()
+
         if serializer.is_valid():
-            serializer.save()
+            # Save the serializer, then explicitly save the instance to the correct database
+            instance = serializer.save()
+            instance._state.db = db
+            instance.save(using=db)
+            
             return Response(serializer.data)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
